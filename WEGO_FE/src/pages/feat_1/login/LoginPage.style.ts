737eaae2434d1e41cb85b670e4ac5187e2ab9f6c
ChanMinLee2 <dlcksals87@gmail.com>
2025-01-07 22:50:48 +0900

    feat: 냅바 적용된 로그인 페이지 기초적 레이아웃 작성

---
+++ WEGO_FE/src/pages/feat_1/login/LoginPage.style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const LoginPageLayout = styled.div`
+  width: 100%;
+  height: 100%;
+  padding-top: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* background-color: green; */
+`;
+
+const ViewContainer = styled.div`
+  width: 95%;
+  height: 100%;
+  /* background-color: bisque; */
+`;
+
+export { LoginPageLayout, ViewContainer };
--- WEGO_FE/src/pages/feat_1/login/LoginPage.tsx
+++ WEGO_FE/src/pages/feat_1/login/LoginPage.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navbar from '../../../components/navbar/Navbar';
+import * as S from './LoginPage.style';
 
 function LoginPage() {
   return (
-    <>
-      <div>로그인 페이지입니다</div>
+    <S.LoginPageLayout>
+      <S.ViewContainer>
+        <div>로그인 페이지입니다</div>
+      </S.ViewContainer>
       <Navbar />
-    </>
+    </S.LoginPageLayout>
   );
 }
 
