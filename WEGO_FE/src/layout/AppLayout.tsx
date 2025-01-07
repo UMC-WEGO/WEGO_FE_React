e836f59e444e6447bff4b920828145707c22de06
ChanMinLee2 <dlcksals87@gmail.com>
2025-01-07 22:26:26 +0900

    feat: 폴더 구조 변경 (각 기능 폴더 하위에 기능에 해당되는 페이지 폴더들이 포함되는 구조), navbar 레이아웃에서 제거하여 필요한 페이지에서 따로 사용

--- WEGO_FE/src/layout/AppLayout.tsx
+++ WEGO_FE/src/layout/AppLayout.tsx
@@ -10,7 +10,7 @@ const RootLayout = () => {
       <S.OutletContainer>
         <Outlet />
       </S.OutletContainer>
-      <Navbar />
+      {/* <Navbar /> */}
     </S.AppContainer>
   );
 };
---
+++ WEGO_FE/src/pages/feat_1/login/LoginPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../../../components/navbar/Navbar';
+
+function LoginPage() {
+  return (
+    <>
+      <div>로그인 페이지입니다</div>
+      <Navbar />
+    </>
+  );
+}
+
+export default LoginPage;
