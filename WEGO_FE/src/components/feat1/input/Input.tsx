b4b6736ad79521be65b0270c529fe3aea2d98410
ChanMinLee2 <dlcksals87@gmail.com>
2025-01-15 22:54:32 +0900

    chore: 공용 input 컴포넌트에 register, signUpInputType, isError 추가, 회원가입 formdata 타입 정의

--- WEGO_FE/src/components/feat1/input/Input.tsx
+++ WEGO_FE/src/components/feat1/input/Input.tsx
@@ -4,10 +4,23 @@ import {
   inputWidthTable,
   inputHeightTable,
 } from '../../../constants/SharedComponentSizeTable';
+import { FieldValues, UseFormRegister, Path } from 'react-hook-form';
+
+type TInputProps<T extends FieldValues> = {
+  // FieldValues는모든 문자열 키에 대해 값이 any 타입인 객체로,
+  // 폼 데이터의 필드 이름과 값들의 형태를 동적으로 처리할 수 있게 함
+  width?: string;
+  height?: string;
+  placeholder: string;
+  register?: UseFormRegister<T>;
+  signUpInputType?: Path<T>; // Path<T>는 타입 T의 키들만을 유니언 타입으로 반환하는 타입
+  isError?: boolean;
+};
 
 interface StyledProps {
   $width?: string; // width 스타일드prop 타입정의
   $height?: string; // height도 정의
+  $isError?: boolean;
 }
 
 const MyInput = styled.input<StyledProps>`
@@ -25,6 +38,7 @@ const MyInput = styled.input<StyledProps>`
   border-radius: 2px;
 
   outline: none;
+  outline: ${props => (props.$isError ? '1px solid #DC0000' : 'none')};
 
   /* Inside auto layout */
   flex: none;
@@ -43,20 +57,28 @@ const MyInput = styled.input<StyledProps>`
   }
 `;
 
-function Input({
+// keyof T는 T 타입에 포함된 모든 키들('email' | 'password' | 'passwordCheck' | 'nickname')을
+// 유니언 타입으로 나타내며, register는 이 값들을 사용할 수 있음
+function Input<T extends FieldValues>({
   width = inputWidthTable.defaultValue,
   height = inputHeightTable.defaultValue,
   placeholder,
-}: {
-  width?: string;
-  height?: string;
-  placeholder: string;
-}) {
+  register,
+  signUpInputType,
+  isError,
+}: TInputProps<T>) {
+  console.log('input signuptype:', signUpInputType);
+  console.log('register:', register);
+  console.log({
+    ...(register && signUpInputType ? register(signUpInputType) : {}),
+  });
   return (
     <MyInput
       $width={width}
       $height={height}
       placeholder={placeholder}
+      // register, signUpInputType이 있을 때만 사용 (옵셔널 prop 로직 처리는 이렇게)
+      {...(register && signUpInputType ? register(signUpInputType) : {})}
     ></MyInput>
   );
 }
--- WEGO_FE/src/pages/feat_1/signup-complete/SignupCompletePage.tsx
+++ WEGO_FE/src/pages/feat_1/signup-complete/SignupCompletePage.tsx
@@ -1,44 +1,9 @@
 import React from 'react';
 import * as S from '../signup/SignupPage.style';
+import * as SS from './SignupCompletePage.style';
 import CloseX from '../../../components/feat1/closeX/CloseX';
 import styled from 'styled-components';
 import Button from '../../../components/feat1/button/Button';
-
-const Re_MainSection = styled(S.MainSection)`
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 60%;
-`;
-
-const SmallText = styled.p`
-  /* 환영합니다! */
-
-  font-family: 'Pretendard';
-  font-style: normal;
-  font-weight: 600;
-  font-size: 14px;
-  line-height: 14px;
-  /* identical to box height, or 100% */
-  text-align: center;
-  letter-spacing: -0.32px;
-
-  color: #8a8a8a;
-`;
-
-const BoldText = styled.p`
-  /* (회원가입 환영 메시지) */
-
-  font-family: 'Pretendard Variable';
-  font-style: normal;
-  font-weight: 700;
-  font-size: 24px;
-  line-height: 36px;
-  /* or 150% */
-  text-align: center;
-
-  color: #000000;
-`;
 
 function SignupCompletePage() {
   return (
@@ -46,13 +11,13 @@ function SignupCompletePage() {
       {/* 여기에서 viewContainer는 form 태그로 생성 */}
       <S.ViewContainer>
         <CloseX></CloseX>
-        <Re_MainSection>
-          <BoldText>
+        <SS.Re_MainSection>
+          <SS.BoldText>
             지금 위고와 <br></br> 즉흥여행을 시작하세요!
-          </BoldText>
-          <SmallText>랜덤 여행지부터 즉흥 미션까지</SmallText>
+          </SS.BoldText>
+          <SS.SmallText>랜덤 여행지부터 즉흥 미션까지</SS.SmallText>
           <Button content="시작하기" color="--color-main-blue" />
-        </Re_MainSection>
+        </SS.Re_MainSection>
       </S.ViewContainer>
     </S.SignUpPageLayout>
   );
