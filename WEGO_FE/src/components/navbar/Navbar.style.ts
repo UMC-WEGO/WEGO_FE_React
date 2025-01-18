5b8d599e725ad626e8b5f896b693e103fbbb5d9b
ChanMinLee2 <dlcksals87@gmail.com>
2025-01-19 00:40:59 +0900

    feat: 냅바 공용 컴포넌트 height 90px로 조정, 및 AppLayout 스타일 컴포넌트 position: relative 추가해서 하위 postion 옵션의 기준으로 설정함

--- WEGO_FE/src/components/navbar/Navbar.style.ts
+++ WEGO_FE/src/components/navbar/Navbar.style.ts
@@ -3,22 +3,19 @@ import styled from 'styled-components';
 const NavbarLayout = styled.div`
   max-width: var(--size-max-width);
   width: 100%;
-  height: var(--size-navbar);
-  position: fixed;
-  bottom: 5%;
+  /* height: var(--size-navbar); */
+  height: 90px;
+  /* position: fixed; */
+  /* bottom: 5%; */
   border-top: 1px solid rgba(165, 165, 165, 1);
-  /* background-color: rgba(
-    165,
-    165,
-    165,
-    0.4
-  ); // navbar가 페이지보다 더 z축에서 위에 표현 됨 */
   border-bottom-left-radius: 25px;
   border-bottom-right-radius: 25px;
-  z-index: 1;
+  z-index: 3;
   display: flex;
   justify-content: center;
   align-items: center;
+  position: absolute;
+  bottom: 0vh;
 `;
 
 const NavbarBox = styled.div`
--- WEGO_FE/src/components/navbar/Navbar.tsx
+++ WEGO_FE/src/components/navbar/Navbar.tsx
@@ -5,16 +5,18 @@ import scrapIcon from '../../images/feat1/Nav_scrap_icon.svg';
 import homeIcon from '../../images/feat1/Nav_home_icon.svg';
 import boardIcon from '../../images/feat1/Nav_board_icon.svg';
 import scheduleIcon from '../../images/feat1/Nav_schedule_icon.svg';
+import { useNavigate } from 'react-router';
 
 function Navbar() {
+  const navigate = useNavigate();
   return (
     <S.NavbarLayout>
       <S.NavbarBox>
-        <img src={scheduleIcon} alt="" />
-        <img src={boardIcon} alt="" />
-        <img src={homeIcon} alt="" />
-        <img src={scrapIcon} alt="" />
-        <img src={myIcon} alt="" />
+        <img src={scheduleIcon} alt="" onClick={() => navigate('/schedule')} />
+        <img src={boardIcon} alt="" onClick={() => navigate('/board')} />
+        <img src={homeIcon} alt="" onClick={() => navigate('/home')} />
+        <img src={scrapIcon} alt="" onClick={() => navigate('/mypage/1')} />
+        <img src={myIcon} alt="" onClick={() => navigate('/mypage/1')} />
       </S.NavbarBox>
     </S.NavbarLayout>
   );
