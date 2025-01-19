d24bb1c0d0e1b2e6e3dabb7f258f41fb0838160a
ChanMinLee2 <dlcksals87@gmail.com>
2025-01-19 19:23:38 +0900

    chore: 스크롤바 디자인 전역으로 설정, 미션 클리어 체크용 이미지 추가, 미션 mockdata 추가 및 타입 정의

---
+++ WEGO_FE/src/mocks/feat1/mission/mockMissions.ts
@@ -0,0 +1,48 @@
+import { TMission } from '../../../types/missionType';
+
+const mockMissions: TMission[] = [
+  {
+    imgSrc: 'missionTestImg1',
+    pointText: '100원',
+    missionName: '골목길 미션',
+    missionDescription: '예쁜 골목길 보이면 사진 찍기',
+    registerDate: '2024.11.28 등록',
+  },
+  {
+    imgSrc: 'missionTestImg2',
+    pointText: '200원',
+    missionName: '공원 산책 미션',
+    missionDescription: '산책 중 마음에 드는 풍경 사진 찍기',
+    registerDate: '2024.12.01 등록',
+  },
+  {
+    imgSrc: 'missionTestImg3',
+    pointText: '150원',
+    missionName: '카페 방문 미션',
+    missionDescription: '예쁜 카페 음료 사진 찍기',
+    registerDate: '2024.12.03 등록',
+  },
+  {
+    imgSrc: 'missionTestImg4',
+    pointText: '300원',
+    missionName: '도서관 미션',
+    missionDescription: '도서관 풍경이나 책 사진 찍기',
+    registerDate: '2024.12.05 등록',
+  },
+  {
+    imgSrc: 'missionTestImg5',
+    pointText: '120원',
+    missionName: '해변 산책 미션',
+    missionDescription: '해변 풍경 사진 찍기',
+    registerDate: '2024.12.07 등록',
+  },
+  {
+    imgSrc: 'missionTestImg6',
+    pointText: '250원',
+    missionName: '야경 촬영 미션',
+    missionDescription: '멋진 야경 사진 찍기',
+    registerDate: '2024.12.10 등록',
+  },
+];
+
+export default mockMissions;
