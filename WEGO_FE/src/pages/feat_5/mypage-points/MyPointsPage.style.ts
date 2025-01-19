b6f46fc23689cb309125b3c440f3dfd502265053
gpdnjs8 <jade3735@naver.com>
2025-01-19 16:45:43 +0900

    feat: 포인트 사용 페이지 상단

---
+++ WEGO_FE/src/pages/feat_5/mypage-points/MyPointsPage.style.ts
@@ -0,0 +1,104 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+  margin-top: 59px;
+  border-top: 1px solid #ddd;
+  border-left: 1px solid #ddd;
+  border-right: 1px solid #ddd;
+  flex-direction: column;
+  margin-left: 13px;
+  margin-right: 13px;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 20px;
+  margin-bottom: 20px;
+  position: relative;
+
+  h1 {
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 20px;
+    letter-spacing: -0.3199999928474426px;
+    text-align: center;
+    text-underline-position: from-font;
+    text-decoration-skip-ink: none;
+    margin: 0px;
+  }
+
+  .arrow-btn {
+    position: absolute;
+    background: none;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    padding: 0;
+    margin: 0;
+    height: 20px;
+  }
+
+  .arrow-btn {
+    left: 7px;
+  }
+
+  .arrow-img {
+    width: 20px;
+    height: 20px;
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
+
+export const PointContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin-left: 6px;
+
+  h1 {
+    font-family: Pretendard;
+    font-size: 18px;
+    font-weight: 600;
+    line-height: 18px;
+    letter-spacing: -0.3199999928474426px;
+    text-align: left;
+    text-underline-position: from-font;
+    text-decoration-skip-ink: none;
+    margin-bottom: 10px;
+  }
+
+  h2 {
+    font-family: Pretendard;
+    font-size: 36px;
+    font-weight: 600;
+    line-height: 36px;
+    letter-spacing: -0.3199999928474426px;
+    text-align: left;
+    text-underline-position: from-font;
+    text-decoration-skip-ink: none;
+    color: #0059ff;
+    margin-bottom: 20px;
+  }
+
+  h3 {
+    font-family: Pretendard;
+    font-size: 15px;
+    font-weight: 500;
+    line-height: 16px;
+    letter-spacing: -0.3199999928474426px;
+    text-align: left;
+    text-underline-position: from-font;
+    text-decoration-skip-ink: none;
+    color: #a5a5a5;
+  }
+`;
--- WEGO_FE/src/pages/feat_5/mypage-points/MyPointsPage.tsx
+++ WEGO_FE/src/pages/feat_5/mypage-points/MyPointsPage.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import * as S from './MyPointsPage.style';
+import { useNavigate, useParams } from 'react-router-dom';
+import Arrow from '../../../images/feat5/Arrow.svg';
+import { users } from '../../../mocks/feat5/UserData';
 
 function MyPointsPage() {
-  return <div>포인트 페이지입니다</div>;
+  const navigate = useNavigate();
+  const { userId } = useParams();
+  const user = users.find(user => user.userId === userId);
+  const points = user ? user.points : '0';
+
+  return (
+    <S.Container>
+      <S.Content>
+        <S.Header>
+          <button className="arrow-btn" onClick={() => navigate(-1)}>
+            <img src={Arrow} alt="Arrow" className="arrow-img" />
+          </button>
+          <h1>포인트 사용</h1>
+        </S.Header>
+        <S.PointContent>
+          <h1>보유 포인트</h1>
+          <h2>{points}P</h2>
+          <h3>구매 시 1일 내로 가입한 이메일로 발송해 드려요.</h3>
+        </S.PointContent>
+      </S.Content>
+    </S.Container>
+  );
 }
 
 export default MyPointsPage;
