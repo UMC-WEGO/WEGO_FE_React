91fc9b96634c271dd601e317faa26cce6fba9bfc
gpdnjs8 <jade3735@naver.com>
2025-01-20 21:47:33 +0900

    feat: 구매 완료 페이지 레이아웃 구현

--- WEGO_FE/src/pages/feat_5/mypage-points/MyPointsCompletePage.tsx
+++ WEGO_FE/src/pages/feat_5/mypage-points/MyPointsCompletePage.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import * as S from './MyPointsCompletePage.style';
+import { useNavigate } from 'react-router-dom';
+import X from '../../../images/feat5/X.svg';
 
 function MyPointsCompletePage() {
-  return <div>포인트구매완료페이지</div>;
+  const navigate = useNavigate();
+  return (
+    <S.Container>
+      <S.Content>
+        <S.Header>
+          <button className="x-btn" onClick={() => navigate(-1)}>
+            <img src={X} alt="x" className="x-img" />
+          </button>
+        </S.Header>
+        <S.PointContent>
+          <h1>구매 완료!</h1>
+          <h2>1일 내 이메일로 구매 상품을 보내드립니다.</h2>
+        </S.PointContent>
+        <S.GoHome>
+          <button onClick={() => navigate('/home')}>홈으로</button>
+        </S.GoHome>
+      </S.Content>
+    </S.Container>
+  );
 }
 
 export default MyPointsCompletePage;
