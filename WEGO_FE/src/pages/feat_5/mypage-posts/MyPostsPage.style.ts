e325271ad6eb32e47b29695fdd85a9e2e6a3a2ec
gpdnjs8 <jade3735@naver.com>
2025-01-22 22:41:05 +0900

    feat: 내가 쓴 글 페이지에서 내가 쓴 글이 없을 때의 화면 구현, 스크롤바 적용

--- WEGO_FE/src/pages/feat_5/mypage-posts/MyPostsPage.style.ts
+++ WEGO_FE/src/pages/feat_5/mypage-posts/MyPostsPage.style.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+interface ContentProps {
+  noScroll?: boolean;
+}
+
 export const Container = styled.div`
   display: flex;
   width: 100%;
@@ -26,7 +30,7 @@ export const Header = styled.header`
     text-align: center;
     text-underline-position: from-font;
     text-decoration-skip-ink: none;
-    margin: 0px;
+    margin-left: -18px;
   }
 
   .arrow-btn {
@@ -50,12 +54,13 @@ export const Header = styled.header`
   }
 `;
 
-export const Content = styled.div`
+export const Content = styled.div.attrs<ContentProps>(props => ({
+  noScroll: props.noScroll,
+}))<ContentProps>`
   display: flex;
   flex-direction: column;
   position: relative;
-
-  overflow-y: scroll;
+  overflow-y: ${props => (props.noScroll ? 'hidden' : 'auto')};
 `;
 
 export const PostWrapper = styled.div`
@@ -74,4 +79,17 @@ export const Button = styled.div`
     width: 16px;
     height: 16px;
   }
+`;
+
+export const NoPostMessage = styled.div`
+  margin-top: 329px;
+  margin-left: -18px;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 16px;
+  letter-spacing: -0.3199999928474426px;
+  text-align: center;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+  color: #0059ff;
 `;
