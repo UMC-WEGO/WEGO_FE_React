faac9d1bd3930c87566a395d59ee3003e3cf9086
gpdnjs8 <jade3735@naver.com>
2025-01-22 22:51:14 +0900

    feat: 지난 여행 페이지 상단바

--- WEGO_FE/src/pages/feat_5/mypage-schedules/MySchedulesPage.tsx
+++ WEGO_FE/src/pages/feat_5/mypage-schedules/MySchedulesPage.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import * as S from './MySchedulesPage.style';
+import { useNavigate } from 'react-router-dom';
+import Arrow from '../../../images/feat5/Arrow.svg';
 
 function MySchedulesPage() {
-  return <div>내 일정 확인 페이지입니다</div>;
+  const navigate = useNavigate();
+  return (
+    <S.Container>
+      <S.Content>
+        <S.Header>
+          <button className="arrow-btn" onClick={() => navigate(-1)}>
+            <img src={Arrow} alt="Arrow" className="arrow-img" />
+          </button>
+          <h1>지난 여행</h1>
+        </S.Header>
+      </S.Content>
+    </S.Container>
+  );
 }
 
 export default MySchedulesPage;
