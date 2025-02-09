2e7f7caec3d2633c1058e8cf7ba44e14109a12c7
gpdnjs8 <jade3735@naver.com>
2025-02-09 23:48:16 +0900

    feat: 사용자 프로필 디폴트 사진 추가

--- WEGO_FE/src/components/feat5/modify/ProfilePicButton.tsx
+++ WEGO_FE/src/components/feat5/modify/ProfilePicButton.tsx
@@ -1,4 +1,5 @@
 import * as S from '../../../pages/feat_5/mypage-profile-modify/MyProfileModifyPage.style';
+import profile_circle from '../../../images/feat5/profile_circle.svg';
 
 interface ProfilePicButtonProps {
   profilePic: string;
@@ -14,7 +15,7 @@ const ProfilePicButton: React.FC<ProfilePicButtonProps> = ({
       {profilePic ? (
         <img src={profilePic} alt="Profile" />
       ) : (
-        <img src={profilePic} alt="Default Profile" />
+        <img src={profile_circle} alt="Default Profile" />
       )}
     </S.ProfilePicButton>
   );
