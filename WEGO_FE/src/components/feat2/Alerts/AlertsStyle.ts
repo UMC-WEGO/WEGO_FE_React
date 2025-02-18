2a796335aabe8842220d040083b1fc32884fd1d3
RileyLooney <rileylooney01@gmail.com>
2025-02-18 17:32:01 +0900

    design: alert 창 디자인

--- WEGO_FE/src/components/feat2/Alerts/AlertsStyle.ts
+++ WEGO_FE/src/components/feat2/Alerts/AlertsStyle.ts
@@ -12,10 +12,10 @@ z-index: 10;
 
 export const CardBox = styled.div`
 width: 270px;
+border: 1px solid black;
 border-radius: 5px;
 
-background-color:rgb(38, 38, 38);
-color: white;
+background-color:rgba(242, 242, 247, 1);
 
 display: flex;
 flex-direction: column;
@@ -29,14 +29,14 @@ z-index: 20; // 배경 위에 표시되도록
 `
 
 export const MessageBox = styled.div`
-border: 1px solid white;
+border-bottom: 1px solid gray;
 width: 100%;
 
 padding: 19px 16px 19px 16px;
 `
 
 export const BtnBox = styled.button`
-border: 1px solid white;
+border: 1px solid gray;
 width: 50%;
 
 padding: 11px;
