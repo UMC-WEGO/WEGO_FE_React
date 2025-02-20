63952b8a19bfdaf2b9ed874f7a9c4a53686c31ba
RileyLooney <rileylooney01@gmail.com>
2025-02-20 23:24:03 +0900

    fix: 인기미션 출력 오류 수정

--- WEGO_FE/src/components/feat2/Post/PostList.tsx
+++ WEGO_FE/src/components/feat2/Post/PostList.tsx
@@ -2,17 +2,30 @@ import PostCard from "./PostCard";
 import { Link } from "react-router-dom";
 
 interface PostForm {
-  id: number;
+  post_id: number;
   picture_url: string;
   category_name: string;
   title: string;
   content: string;
   location_name: string;
   created_at: string;
-  conmment_count: number;
-  like_count: number;
-  scrap_count: number;
+
+  total_comment: number;
+  total_like: number;
+  total_scrap: number;
   popularity_score: string;
+
+//   category_name: "현지 정보"
+//   content: "순천 안온해변에서 어쩌고"
+//   created_at: "2025-02-17T04:23:20.000Z"
+//   location_name: "군산"
+//   picture_url: null
+//   popularity_score:"19.5"
+// - post_id: 93
+//   title: "이런건 어떤가요"
+// - total_comment: 5
+// - total_like: 1
+// - total_scrap: 1
 }
 
 interface PostListForm {
@@ -27,7 +40,7 @@ const PostList = ({posts, limit, showRanking = false}: PostListForm) => {
   return(
     <div>
       {displayPosts.slice(0,3).map((postData, index) => (
-        <Link key={postData.id} to={`/board/detail/${postData.id}`}>
+        <Link key={postData.post_id} to={`/board/detail/${postData.post_id}`}>
           <PostCard
             img_url={postData.picture_url}
             tag={postData.category_name}
@@ -35,9 +48,9 @@ const PostList = ({posts, limit, showRanking = false}: PostListForm) => {
             content={postData.content}
             location={postData.location_name}
             timestamp={postData.created_at}
-            likes_num={postData.like_count}
-            comments_num={postData.conmment_count}
-            scripts_num={postData.scrap_count}
+            likes_num={postData.total_like}
+            comments_num={postData.total_comment}
+            scripts_num={postData.total_scrap}
           />
         </Link>
       ))}
