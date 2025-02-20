613b0423edd5aa1e9f33c370ec5a7a6219ef4176
gpdnjs8 <jade3735@naver.com>
2025-02-20 16:57:00 +0900

    fix: navbar 마이페이지 userId 오류 수정

--- WEGO_FE/src/pages/feat_5/mypage/MyPage.tsx
+++ WEGO_FE/src/pages/feat_5/mypage/MyPage.tsx
@@ -1,7 +1,7 @@
 import * as S from './MyPage.style';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import Profile from '../../../components/feat5/MypageHome/Profile';
 import ProfileMenu from '../../../components/feat5/MypageHome/ProfileMenu';
 import TempContainer from '../../../components/feat5/MypageHome/Temperature';
@@ -11,10 +11,11 @@ import { userinfoApis } from '../../../apis/feat5/userinfoApis';
 import Loading from '../../../components/feat5/Loading';
 import ErrorMessage from '../../../components/feat5/ErrorMessage';
 import Navbar from '../../../components/navbar/Navbar';
-import { useTokenStore } from '../../../store/token/useTokenStore';
 
 function MyPage() {
   const navigate = useNavigate();
+  const { userId } = useParams();
+  const urlUserId = userId; // url에 있는 userId
   const [showLogoutModal, setShowLogoutModal] = useState(false);
 
   // API
@@ -23,25 +24,26 @@ function MyPage() {
     queryFn: userinfoApis,
   });
 
-  useEffect(() => {
-    if (!isLoading && data) {
-      useTokenStore.setState({
-        data: {
-          userId: data.user_id || '', // 기본값 설정
-          loginApiReqData: data.loginApiReqData || {},
-          accessToken: data.accessToken || '',
-          refreshToken: data.refreshToken || '',
-        },
-      });
-    }
-  }, [data, isLoading]);
-
   // 로딩, 에러 처리
   if (isLoading) return <Loading />;
   if (error instanceof Error) return <ErrorMessage error={error} />;
 
   console.log('API 받은 데이터', data);
 
+  // user_id가 다른 경우 접근 처리
+  if (data && urlUserId !== String(data.user_id)) {
+    return (
+      <S.Container>
+        <S.Header>
+          <h1>마이페이지</h1>
+        </S.Header>
+        <p className="noaccess">
+          잘못된 접근입니다.
+          <span className="line">다시 시도해주세요!</span>
+        </p>
+      </S.Container>
+    );
+  }
   // 경로 이동
   const handleNavigate = (path: string) => {
     if (data) {
