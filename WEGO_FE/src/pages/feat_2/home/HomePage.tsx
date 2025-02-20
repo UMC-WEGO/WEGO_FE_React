048a7b3a5efed675e7ecdf141305223764537b48
RileyLooney <rileylooney01@gmail.com>
2025-02-20 22:07:14 +0900

    refactor: home화면 API 수정

--- WEGO_FE/src/pages/feat_2/home/HomePage.tsx
+++ WEGO_FE/src/pages/feat_2/home/HomePage.tsx
@@ -22,6 +22,7 @@ import ModalMessage from '../../../components/feat2/Modal';
 
 import { TOKEN } from '../../../mocks/feat2/TOKEN_Temporary_file';
 import StartPage from '../home-start/StartPage';
+import { authInstance } from '../../../apis/axiosInstance';
 
 // 
 // 
@@ -77,12 +78,15 @@ function HomePage() {
   useEffect(() => {
     const getUpcomingTrip = async() => {
       try {
-        const responseTravel: AxiosResponse<any> = await axios.get(`http://13.124.213.122:3000/home/upcoming-trips`, {
-          headers: {
-            Authorization: `${TOKEN}`,
-            Accept: `application/json`
-          }
-        })
+        // const responseTravel = await axios.get(`http://13.124.213.122:3000/home/upcoming-trips`, {
+        //   headers: {
+        //     Authorization: `${TOKEN}`,
+        //     Accept: `application/json`
+        //   }
+        // })
+
+        const responseTravel = await authInstance.get(`http://13.124.213.122:3000/home/upcoming-trips`)
+
         setUpcomingTravelList(responseTravel.data.result);
         setUpcomingTravelMessage(responseTravel.data.message);
         setLoadingTravel(false);
@@ -96,7 +100,7 @@ function HomePage() {
           setIsShowModal(true);
         }
       } catch(error) {
-        setErrorTravel('Error fetching data');
+        setErrorTravel(' --- --- --- Error On 다가오는 여행 조회회');
         setLoadingTravel(false);
       }
     }
@@ -108,19 +112,21 @@ function HomePage() {
 // --- --- --- 다가오는 일정 삭제 --- --- ---
   const deleteUpcomingTravel = async(tripId: number) => {
     try {
-      const responseDeleteTravel = await axios.delete(`http://13.124.213.122:3000/home/upcoming-trips/${tripId}`,{
-        headers: {
-          Authorization: `${TOKEN}`,
-          Accept: `application/json`,
-          'Content-Type': 'application/json',
-        }
-      })
+      // const responseDeleteTravel = await axios.delete(`http://13.124.213.122:3000/home/upcoming-trips/${tripId}`,{
+      //   headers: {
+      //     Authorization: `${TOKEN}`,
+      //     Accept: `application/json`,
+      //     'Content-Type': 'application/json',
+      //   }
+      // })
+
+      const responseDeleteTravel = await authInstance.get(`http://13.124.213.122:3000/home/upcoming-trips/${tripId}`)
 
       // console.log("삭제 성공 여부 : ", responseDeleteTravel);
 
       setUpcomingTravelList((prevPlanList) => prevPlanList.filter((plan) => plan.tripId !== tripId))
     } catch (error) {
-      // console.log(error);
+      console.log(" --- --- --- Error On 일정 삭제")
     }
   }
 
@@ -132,17 +138,21 @@ function HomePage() {
   useEffect(() => {
     const getPopularPost = async() => {
       try {
-        const responseGet = await axios.get(`http://13.124.213.122:3000/community/popular-posts`, {
-          headers: {
-            Authorization: `${TOKEN}`,
-            Accept: `application/josn`
-          }
-        })
+        // const responseGet = await axios.get(`http://13.124.213.122:3000/community/popular-posts`, {
+        //   headers: {
+        //     Authorization: `${TOKEN}`,
+        //     Accept: `application/josn`
+        //   }
+        // })
+
+        const responseGet = await authInstance.get(`http://13.124.213.122:3000/community/popular-posts`)
+
         setPopularPostList(responseGet.data);
         setLoadingPost(false);
 
         console.log("인기 게시물 조회 결과 : ", responseGet);
       }  catch(error) {
+        console.log('Error On 인기 게시물 조회')
         setErrorPost('Error on importing posts')
       }
     }
@@ -155,14 +165,21 @@ function HomePage() {
 
   useEffect(() => {
     const getPopularMission = async() => {
-      const responseMission = await axios.get(`http://13.124.213.122:3000/home/popular-missions`, {
-        headers: {
-          Authorization: `${TOKEN}`,
-          Accept: `application/josn`
-        }
-      })
-      setPopularMissionList(responseMission.data.result.missions)
-      console.log("인기 미션 조회 결과 : ",responseMission)
+      try{
+        // const responseMission = await axios.get(`http://13.124.213.122:3000/home/popular-missions`, {
+        //   headers: {
+        //     Authorization: `${TOKEN}`,
+        //     Accept: `application/josn`
+        //   }
+        // })
+
+        const responseMission = await authInstance.get(`http://13.124.213.122:3000/home/popular-missions`)
+
+        setPopularMissionList(responseMission.data.result.missions)
+        console.log("인기 미션 조회 결과 : ",responseMission)        
+      } catch (error) {
+        console.log('Error On 인기 미션 조회')
+      }
     }    
     getPopularMission();
   }, [])
